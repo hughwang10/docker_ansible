537b079c2dcb7ef82b435f8885c62872261b08e7
hughwang10 <hughwang10@gmail.com>
2017-09-28 17:01:31 +0100

    updates

--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# example
 docker run -it \
-  -v /home/hugh/myshare/projects/TIM_AFG/live_development/out_result/site_spec:/opt/miep/tools/miit/msp_miit/site-config/tmp/site_spec \
-  -v /home/hugh/myshare/projects/TIM_AFG/live_development/out_result/vars:/opt/miep/tools/miit/msp_miit/site-config/tmp/vars \
-  -v /home/hugh/result:/opt/miep/tools/miit/msp_miit/site-config/result \
-  -v /home/hugh/myshare/projects/TIM_AFG/live_development/out_result/miit_cmd.sh:/opt/miep/tools/miit/msp_miit/site-config/tmp/miit_cmd.sh \
-  --rm --name test \
+  -v /media/sf_myshare/projects/vf_Ireland_2017/SA_680AFGT/out_result/site_spec:/opt/miep/tools/miit/msp_miit/site-config/tmp/site_spec \
+  -v /media/sf_myshare/projects/vf_Ireland_2017/SA_680AFGT/out_result/vars:/opt/miep/tools/miit/msp_miit/site-config/tmp/vars \
+  -v /media/sf_myshare/projects/vf_Ireland_2017/SA_680AFGT/out_result:/opt/miep/tools/miit/msp_miit/site-config/result \
+  -v /media/sf_myshare/projects/vf_Ireland_2017/archive:/opt/miep/tools/miit/system-images/archive \
+  -v /media/sf_myshare/projects/vf_Ireland_2017/SA_680AFGT/miit_cmd.sh:/opt/miep/tools/miit/msp_miit/site-config/tmp/miit_cmd.sh \
+  -v /media/sf_myshare/projects/vf_Ireland_2017/SA_680AFGT/test.yml:/opt/miep/tools/miit/msp_miit/site-config/test.yml \
+  --rm --name 680AFGT \
   --network host  \
   hugh/toolsvm_ansible:1.9 \
   /bin/bash
