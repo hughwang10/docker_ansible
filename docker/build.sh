d6a8578fca6a345f8a2de357846cf3b4e7aa5454
hughwang10 <hughwang10@gmail.com>
2017-10-13 10:24:38 +0100

    updates

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #docker build --no-cache --network host -t hugh/toolsvm_ansible:1.9 .
-docker build --network host -t hugh/toolsvm_ansible:1.9 .
+docker build --network host -t hugh/toolsvm_ansible_17ga:1.9 .
 docker rmi $(docker images -q --filter "dangling=true")
 docker images
