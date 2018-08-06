ba394714cd96f4f056050456c843388d66724882
hughwang10 <hughwang10@gmail.com>
2018-08-06 20:37:49 +0100

    update

--- docker/action_cmd.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ansible-play -i inventory \
--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# example
 docker run -it \
 -v `pwd`:/project \
---rm --name afg-toolsvm \
---network host  \
-hugh/ansible_docker \
-# /bin/bash
+--rm --name afg-tools \
+--network host \
+hugh/ansible_docker
